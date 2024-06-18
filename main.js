ff3f0d68ca26accb31eb5d9b078344555e2b8aab
Jenny <156132099+Jennyznz@users.noreply.github.com>
2024-06-18 17:38:47 -0400

    Records and returns user input

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-console.log("Hello World");
+console.log(getHumanChoice());
+
+function getComputerChoice() {
+    let choice = "";
+    let rand = (Math.random() * 100);
+    if (rand < 34) {
+        choice = "rock";
+    }
+    else if (rand < 67) {
+        choice = "paper";
+    }
+    else {
+        choice = "scissors";
+    }
+    return choice;
+}
+
+function getHumanChoice() {
+    let choice = "";
+    while (choice == "") {           // in case the user misspells
+        let input = prompt("rock, paper, or scissors?");
+        input = input.trim().toLowerCase();        // eliminates extraneous spacing and capitalizations
+        if ((input == "rock") || (input == "paper") || (input == "scissors")){
+            choice = input;
+        }
+    }
+    return choice;
+}
